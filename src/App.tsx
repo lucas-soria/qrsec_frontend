cf1f4b909746a56a29bd081c085ec4349e4e982b
Lucas Soria <lucasdsg00@gmail.com>
2025-02-06 19:58:56 -0300

    feat: added temporary drawer to small screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { frontUrls } from './data/Urls';
 import { SendInvite } from './pages/SendInvite';
 import { Layout as PermanentDrawer } from './components/Layout/LogedInPermanentDrawer';
-// import { Layout as TemporalDrawer } from './components/Layout/LogedInTemporalDrawer';
+import { Layout as TemporalDrawer } from './components/Layout/LogedInTemporalDrawer';
 import { Layout as AppBar } from './components/Layout/NotLogedInAppBar';
 import { CreateNewAddress } from './pages/CreateAddress';
 import { ListAddresses } from './pages/ListAddresses';
@@ -15,56 +15,68 @@ import { ViewGuest } from './pages/ViewGuest';
 import { ViewInvite } from './pages/ViewInvite';
 import { ViewPublicInvite } from './pages/ViewPublicInvite';
 import { ViewUser } from './pages/ViewUser';
+import { useMediaQuery } from '@mui/material';
 
 export function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
 
-        <Route element={ <AppBar /> }>
+  	const isMobile = useMediaQuery("(max-width:800px)");
 
-            { /* Session */ }
-            <Route path={ frontUrls.signup } element={ <SignUp/> } />
-            <Route path={ frontUrls.signin } element={ <SignUp/> }/>
-            <Route path={ frontUrls.base } element={ <Navigate replace to={ frontUrls.signup }/> }/>
+  	return (
+		<BrowserRouter>
+			<Routes>
 
-            { /* Invites */ }
-            <Route path={ frontUrls.publicInvite + ':id' } element={ <ViewPublicInvite/> }/>
+				<Route element={ <AppBar /> }>
 
-        </Route>
+					{ /* Session */ }
+					<Route path={ frontUrls.signup } element={ <SignUp/> } />
+					<Route path={ frontUrls.signin } element={ <SignUp/> }/>
+					<Route path={ frontUrls.base } element={ <Navigate replace to={ frontUrls.signup }/> }/>
 
-        <Route element={ <PermanentDrawer /> }>
+					{ /* Invites */ }
+					<Route path={ frontUrls.publicInvite + ':id' } element={ <ViewPublicInvite/> }/>
 
-            { /* Invites */ }
-            <Route path={ frontUrls.invite } element={ <ListInvites/> }/>
-            <Route path={ frontUrls.invite + frontUrls.create } element={ <SendInvite/> }/>
-            <Route path={ frontUrls.invite + ':id' } element={ <ViewInvite/> }/>
-            <Route path={ frontUrls.scan } element={ <ScanInvite/> }/>
+				</Route>
 
-            { /* Addresses */ }
-            <Route path={ frontUrls.address } element={ <ListAddresses/> }/>
-            <Route path={ frontUrls.address + frontUrls.create } element={ <CreateNewAddress/> }/>
+				<Route element={ 
+					<>
+						{isMobile ? (
+							<TemporalDrawer />
+						):
+							<PermanentDrawer />
+						}
+					</>
+				} >
 
-            { /* Users */ }
-            <Route path={ frontUrls.user } element={ <ListUsers/> }/>
-            <Route path={ frontUrls.user + ':id' } element={ <ViewUser/> }/>
+					{ /* Invites */ }
+					<Route path={ frontUrls.invite } element={ <ListInvites/> }/>
+					<Route path={ frontUrls.invite + frontUrls.create } element={ <SendInvite/> }/>
+					<Route path={ frontUrls.invite + ':id' } element={ <ViewInvite/> }/>
+					<Route path={ frontUrls.scan } element={ <ScanInvite/> }/>
 
-            { /* Guests */ }
-            <Route path={ frontUrls.guest } element={ <ListGuests/> }/>
-            <Route path={ frontUrls.guest + ':id' } element={ <ViewGuest/> }/>
+					{ /* Addresses */ }
+					<Route path={ frontUrls.address } element={ <ListAddresses/> }/>
+					<Route path={ frontUrls.address + frontUrls.create } element={ <CreateNewAddress/> }/>
 
-            {
-            /*  Original es con rutas protegidas
-            <Route element={ <ProtectedRoutes/> }>
-                <Route path={ frontUrls.create } element={ <SendInvite/> }/>
-                <Route path={ frontUrls.scan } element={ <ScanInvite/> }/>
-            </Route>
-            */
-            }
+					{ /* Users */ }
+					<Route path={ frontUrls.user } element={ <ListUsers/> }/>
+					<Route path={ frontUrls.user + ':id' } element={ <ViewUser/> }/>
 
-        </Route>
+					{ /* Guests */ }
+					<Route path={ frontUrls.guest } element={ <ListGuests/> }/>
+					<Route path={ frontUrls.guest + ':id' } element={ <ViewGuest/> }/>
 
-      </Routes>
-    </BrowserRouter>
-  );
+					{
+					/*  Original es con rutas protegidas
+					<Route element={ <ProtectedRoutes/> }>
+						<Route path={ frontUrls.create } element={ <SendInvite/> }/>
+						<Route path={ frontUrls.scan } element={ <ScanInvite/> }/>
+					</Route>
+					*/
+					}
+
+				</Route>
+
+			</Routes>
+		</BrowserRouter>
+	);
 };
--- src/components/Layout/LogedInTemporalDrawer.tsx
+++ src/components/Layout/LogedInTemporalDrawer.tsx
@@ -1,122 +1,62 @@
-import LogoutIcon from '@mui/icons-material/Logout';
-import { Avatar, Box, Button, Container, Drawer, Link, List, ListItem, MenuItem, useMediaQuery } from '@mui/material';
-import jwt_decode from 'jwt-decode';
-import { useState } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { AppBar, Box, Container, Drawer, IconButton, Toolbar } from '@mui/material';
+import { Outlet } from 'react-router-dom';
 import { frontUrls } from '../../data/Urls';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 
 export function Layout() {
 
     let qrsecLogo = '/QRSec logo.svg';
 
-    const navigate = useNavigate();
-
-    var token : string = localStorage.getItem('access_token') ?? '';
-    
-        var decoded_token : GoogleJWT | null = !!token ? jwt_decode(token) : null;
-    
-        var user : [string, string] = !!decoded_token ? [ decoded_token.given_name, decoded_token.family_name ] : [ '', '' ];
-
-    const pages : string[][] = [
-        [ frontUrls.wholeBase + frontUrls.invite, 'Invitaciones'],
-        [ frontUrls.wholeBase + frontUrls.guest, 'Invitados'],
-        [ frontUrls.wholeBase + frontUrls.scan, 'Escanear'],
-        [ frontUrls.wholeBase + frontUrls.user, 'Usuarios'],
-        [ frontUrls.wholeBase + frontUrls.address, 'Direcciones'],
-    ]
-
-    let location = useLocation();
-
-    const isSelected = (link : string) : string => {
-        if (frontUrls.wholeBase + location.pathname.substring(1, location.pathname.length) === link) {
-            return 'selected'
-        }
-        return 'not-selected'
-    }
-
-    const stringAvatar = (name : [string, string]) => {
-        
-        return { children: `${name[0][0]}${name[1][0]}` };
-
-    }
-
-    const handleLogOut = () => {
-        
-        localStorage.removeItem('access_token');
-        navigate(frontUrls.base);
-
-    }
-    
     const [open, setOpen] = useState(false);
-    const isMobile = useMediaQuery("(max-width:800px)");
+
+    const handleClick = () => {
+        
+        setOpen(!open);
+
+    }
 
     return (
         <div id='layout' >
 
             
-            {/* Drawer */}
-            <div id={isMobile ? 'temporary-drawer' : 'drawer'}>
-            {isMobile &&
-                <Button onClick={() => setOpen(true)}>Open Menu</Button>
-            }
-                <Drawer
-                    variant = {isMobile ? 'temporary' : 'permanent'}
-                    anchor = 'left'
-                    open={open}
-                    onClose={() => setOpen(false)}
-                    PaperProps = {
-                        {
-                            style: {
-                                position: 'inherit',
-                                backgroundColor: 'inherit',
-                                border: 0,
-                                width: 'inherit',
-                            }
-                        }
-                    }>
-                <Box
-                    sx={
-                        {
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            flexDirection: 'column',
-                            py: 3,
-                        }
-                    }
-                >
-                    <a href={ frontUrls.wholeBase } ><img className='logo' id='logo' src={ qrsecLogo } alt='QRSec Logo' /></a>
-                </Box>
-                <List>
-                    {pages.map( (page) => (
-                        <ListItem key={ page[1] } id='drawer-item' className={isSelected(page[0])}>
-                            <Link id='drawer-link' href={ page[0] } textAlign='center' >{ page[1] }</Link>
-                        </ListItem>
-                    ))}
-                </List>
-                <div className='bottom-drawer'>
-                
-                    <Avatar className='avatar' src={decoded_token?.picture} { ...stringAvatar(user) } />
-                
-                    <MenuItem key={ 'log-out' } onClick={ handleLogOut } style={
-                        {
-                            display:'flex',
-                            justifyContent:'flex-end',
-                            justifyItems:'flex-end',
-                            marginLeft:'auto',
-                        }
-                        }>
-                        <Box>
-                            <LogoutIcon className='button-icon' />
+            {/* App Bar */}
+            <div>
+                <AppBar id='navbar' enableColorOnDark >
+                    <Toolbar>
+                        <IconButton
+                            size='large'
+                            edge='start'
+                            color='inherit'
+                            aria-label='menu'
+                            sx={{ mr: 2 }}
+                            onClick={ handleClick }
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Box display='flex' alignItems='center' gap={2}>
+                            <a href={ frontUrls.wholeBase } ><img id='navbar-logo' src={ qrsecLogo } alt='QRSec Logo' /></a>
                         </Box>
-                    </MenuItem>
+                    </Toolbar>
+                </AppBar>
+            </div>
+
+            <div>
+            <Drawer
+                anchor='left'
+                open={open}
+                onClose={() => setOpen(false)}
+                variant={'temporary'}
+            >
+                <div style={{ width: 250, padding: 20 }}>
+                    <p>Menu Content</p>
                 </div>
-                </Drawer>
+            </Drawer>
             </div>
 
             {/* Main Content */}
-            <div id="content-with-drawer">
-                <Container maxWidth='sm' fixed>
+            <div id='content-with-navbar'>
+                <Container maxWidth='sm' >
                     <Outlet />
                 </Container>
             </div>
